18efa38643fe2b0c36e7fe758c62dedfbc3fb3eb
OrJaku <kuba.orzel@hotmail.com>
2019-06-29 21:20:48 +0200

    Możliwość dodawania produktów do listy

--- Zad1.py
+++ Zad1.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
 Zad1 = Flask(__name__)
 list_p = []
 @Zad1.route('/')
 def home():
-    return render_template("home.html")
-
-
-@Zad1.route('/prodacts')
-def prodacts():
     return render_template("prodacts.html")
-    r = request.get(Name)
-    list_p.append(r)
-@Zad1.route('/shop')
+
+
+@Zad1.route('/prodacts', methods=['POST'])
+def prodacts():
+    prodact = request.form['prodact']
+    list_p.append(prodact)
+    return redirect(url_for('home'))
+
+
+
+@Zad1.route('/shop', methods=['GET'])
 def shop():
-    return render_template("shop.html", list_url=list_p)
+
+    return render_template("shop.html", list_p = list_p)
