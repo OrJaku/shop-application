6712a381891405b6842c299b14e918a3889a606f
OrJaku <kuba.orzel@hotmail.com>
2019-07-25 09:00:40 +0200

    Ordered application files, moved all endpoint to views.py

--- app/models.py
+++ app/models.py
@@ -9,4 +9,5 @@ class Product(db.Model):
     price = db.Column(db.Float())
 
     def __repr__(self):
-        return f'Product <id {self.id}><name {self.name}><price {self.price}'
+
+        return f'Name: {self.name}   Price: {self.price}   [ID:{self.id}]'
