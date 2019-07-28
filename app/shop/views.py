b8be2b4f9e675b46e729f2bd941574e329f6b776
OrJaku <kuba.orzel@hotmail.com>
2019-07-28 13:53:52 +0200

    Flash messages added

--- app/shop/views.py
+++ app/shop/views.py
@@ -1,6 +1,6 @@
 from . import shop
 from .. import db
-from flask import render_template, request, url_for, redirect
+from flask import render_template, request, url_for, redirect, flash
 from ..models import Product
 from colorama import Fore, Style  # test
 
@@ -26,8 +26,12 @@ def add():
     product_name = request.form['pr_name']
     product_price = request.form['pr_price']
     new_product = Product(name=product_name, price=product_price)
-    db.session.add(new_product)
-    db.session.commit()
+    if product_name == '' or product_price == '':
+        flash('Enter product name and price', 'error')
+    else:
+        db.session.add(new_product)
+        flash('Product %s (price: %s) hes been added' % (product_name, product_price), 'succes')
+        db.session.commit()
     return redirect(url_for('shop.products'))
 
 
@@ -37,9 +41,11 @@ def delete():
     if product_name == "del_all_prod":
         Product.query.filter().delete()
         db.session.commit()
+        flash('All products hes been removed from list', 'error')
     else:
         Product.query.filter_by(name=product_name).delete()
-        print("Product", product_name)
+        print("Product %s hes been removed" % product_name)
+        flash('Product %s hes been removed' % product_name, 'succes')
         db.session.commit()
     return redirect(url_for('shop.remove'))
 
@@ -53,8 +59,11 @@ def shop_list():
     products_price = ([x[0] for x in products_price])
     products_id = db.session.query(Product.id).all()
     products_id = ([x[0] for x in products_id])
-    print(Fore.GREEN + 'PR_NAME', products_list)  # test
+    print(Fore.YELLOW + 'PR_Name', products_name)  # test#
     print(Style.RESET_ALL)  # test
+    if not products_name:
+        flash('Product list is empty, add product to list', 'info')
+        print('List is empty')
     return render_template("shop_list.html", products_list=products_list,
                            products_name=products_name, products_price=products_price,
                            products_id=products_id)
